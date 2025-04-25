2990f0111635c1bd615cf4f1d1a94dca70374c37
Ionni16 <ionut1.puiu@gmail.com>
2025-04-25 23:05:46 +0200

    adsense

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,7 +19,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="it">
+      <head>
+      {/* Script di Google AdSense */}
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3402722098398750"
+     crossorigin="anonymous"></script>
+     </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
